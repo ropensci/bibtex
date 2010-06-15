0deaa84328d754424bc7cb6939ac4f128ac370ae
Romain François <romain@r-enthusiasts.com>
2010-06-15 09:14:13 +0000

    adapt to changes in R

--- R/bibtex.R
+++ R/bibtex.R
@@ -1,3 +1,68 @@
+
+R2.12.0 <- getRversion() >= "2.12.0"
+
+.bibentry <- if(R2.12.0) {
+	get( "bibentry", asNamespace( "utils" ) )
+} else {
+	function( bibtype, textVersion, header = NULL, 
+		footer = NULL, key = NULL, ..., other = list(), 
+		mheader = NULL, mfooter = NULL ){
+		
+		y <- list(...)
+		if( "author" %in% names(y) ){
+			y[["author"]] <- as.personList( y[["author"]] )
+		}
+		structure( y,
+			entry = bibtype, 
+			key = key, 
+			class = "citation" 
+			)
+	}
+}
+
+make.bib.entry <- function( x ){
+		type <- attr( x, "entry" )
+		key  <- attr( x, "key" )
+		
+		y <- as.list( x )
+		
+		textVersion <- sprintf( "%s. %s (%s)", 
+			if( "author" %in% names(x) ) x[["author"]] else "?", 
+			if( "title" %in% names(x) ) x[["title"]] else "?", 
+			if( "year" %in% names(x) ) x[["year"]] else "?"
+			)
+		
+		
+		tryCatch(  
+			.bibentry( bibtype = type, key = key, textVersion = textVersion ,
+				other = y ), 
+		error = function(e){
+			message( sprintf( "ignoring entry '%s' (line %d) because :\n\t%s\n", 
+				key, 
+				attr(x, "srcref")[1], 
+				conditionMessage( e ) ) )
+			NULL
+		} )
+}
+
+make.citation.list <- if( R2.12.0 ){
+	function( x, header, footer) {
+		rval <- list()
+		for( i in seq_along(x) ){
+			if( !is.null(x[[i]] ) )
+				rval <- c( rval, x[[i]] )
+		}
+		class(rval) <- c( "citation", "bibentry" )
+		rval
+	}  
+} else {
+	function( x , header, footer ){
+		structure( out, class = "citationList", 
+			header = header, footer = footer )
+	}
+}
+
+
 read.bib <- function(
 	file = system.file( "REFERENCES.bib", package = package ), 
 	package = "bibtex", 
@@ -14,20 +79,11 @@ read.bib <- function(
 	out <- .External( "do_read_bib", file = file, 
 		encoding = encoding, srcfile = srcfile )
 	at  <- attributes(out) 
-	out <- lapply( out, function(x){
-		y <- as.list( x )
-		if( "author" %in% names(y) ){
-			y[["author"]] <- as.personList( y[["author"]] )
-		}
-		structure( y, class = "citation", 
-			srcref = attr( x, "srcref" ), 
-			entry = attr( x, "entry" ), 
-			key = attr( x, "key") ) 
-	} )
-	preamble <- at[["preamble"]] 
-	structure( out, class = "citationList", 
-		header = header, 
-		footer = footer, 
-		strings = at[["strings"]] )
+	out <- lapply( out, make.bib.entry )
+	preamble <- at[["preamble"]]
+	
+	out <- make.citation.list( out, header, footer )
+	# attr( out, "strings") <- at[["strings"]]
+	out
 }
 
