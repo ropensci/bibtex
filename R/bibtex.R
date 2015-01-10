8a9d2cc0932f1bbf3ab0db6d3bc5a9ba787bdb50
mwmclean <mathew.w.mclean@gmail.com>
2015-01-10 03:22:27 -0600

    added author parsing from RefManageR

--- R/bibtex.R
+++ R/bibtex.R
@@ -29,6 +29,126 @@ function( x )
     as.personList( authors )
 }
 
+UnlistSplitClean <- function(s)
+  unlist(strsplit(gsub("[{}]", "", str_trim(s)), " "))
+
+
+cleanupLatex <- function (x){
+  if (!length(x))
+    return(x)
+
+  if (any(grepl('mkbib', x))){
+    x <- gsub('mkbibquote', 'dQuote', x)
+    x <- gsub('mkbibemph', 'emph', x)
+    x <- gsub('mkbibbold', 'bold', x)
+  }
+  x <- gsub('\\\\hyphen', '-', x)
+
+  latex <- try(tools::parseLatex(x), silent = TRUE)
+  if (inherits(latex, "try-error")) {
+    x
+  }else {
+    x <- tools::deparseLatex(tools::latexToUtf8(latex), dropBraces = TRUE)
+    if (grepl("\\\\[[:punct:]]", x)){
+      x <- gsub("\\\\'I", '\u00cd', x)
+      x <- gsub("\\\\'i", '\u00ed', x)
+      x <- gsub('\\\\"I', '\u00cf', x)
+      x <- gsub('\\\\"i', '\u00ef', x)
+      x <- gsub("\\\\\\^I", '\u00ce', x)
+      x <- gsub("\\\\\\^i", '\u00ee', x)
+      x <- gsub("\\\\`I", '\u00cc', x)
+      x <- gsub("\\\\`i", '\u00ec', x)
+      Encoding(x) <- 'UTF-8'
+    }
+    x
+  }
+}
+
+ArrangeAuthors <- function (x){
+  rx <- "[[:space:]]+and[[:space:]]+"
+  x <- gsub('[[:space:]]{2,}', ' ', x, useBytes = TRUE)
+  authors <- lapply(strsplit(x, rx)[[1]], ArrangeSingleAuthor)
+  as.personList(authors)
+}
+
+#' @importFrom stringr str_trim
+ArrangeSingleAuthor <- function(y){
+
+  if (grepl('[\\]', y)){
+    tmp <- try(parseLatex(y), TRUE)
+    if (!inherits(tmp, 'try-error'))
+      y <- deparseLatex(latexToUtf8(tmp))
+  }
+  parts <- unlist(strsplit(y, ','))
+  len.parts <- length(parts)
+  if (len.parts == 1L){
+    #     parts <- "{Barnes} {and} {Noble,} {Inc.}"
+    if (grepl("[^{][[:print:]][}]$", parts)){
+      s <- unlist(strsplit(parts, ''))
+      i <- length(s) - 1L
+      paren <- 1
+      while (paren > 0 && i > 0){
+        if (s[i] == '{'){
+          paren <- paren - 1L
+        }else if (s[i] == '}'){
+          paren <- paren + 1L
+        }
+        i <- i - 1L
+      }
+      last <- paste0(s[(i+2):(length(s)-1)], collapse = '')
+      first <- NULL
+      if (i > 0)
+        first <- paste0(s[seq_len(i-1)], collapse = '')
+      person(UnlistSplitClean(first), cleanupLatex(last))  # Mathew {McLean IX}
+    }else{
+      vonrx <- "(^|[[:space:]])([[:lower:]+[:space:]?]+)[[:space:]]"
+      m <- regexec(vonrx, parts)
+      von <- unlist(regmatches(parts, m))[3L]
+      if (!is.na(von)){
+        name <- unlist(strsplit(parts, vonrx))
+        if (length(name) == 1L){  # von Bommel
+          person(family=c(cleanupLatex(von), cleanupLatex(name)))
+        }else{  # Mark von Bommel
+          person(given = UnlistSplitClean(name[1L]), family=c(cleanupLatex(von), cleanupLatex(name[2L])))
+        }
+      }else{  # George Bernard Shaw
+        name <- UnlistSplitClean(parts)
+        len.name <- length(name)
+        if (len.name <= 1L){
+          person(family = name)
+        }else{
+          person(given = name[seq_len(len.name - 1L)], family = name[len.name])
+        }
+      }
+    }
+  }else if (len.parts == 2L){
+    if (grepl('^[{]', parts[1L])){  # e.g. {de Gama}, Vasco
+      person(UnlistSplitClean(parts[2L]), UnlistSplitClean(parts[1L]))
+    }else{
+      vonrx <- "^([[:lower:]+[:space:]?]+)[[:space:]]"
+      m <- regexec(vonrx, parts[1L])
+      von <- unlist(regmatches(parts[1L], m))[2]
+      if (is.na(von)){  # e.g. Smith, John Paul
+        person(UnlistSplitClean(parts[2L]), cleanupLatex(parts[1L]))
+      }else{  # e.g. de la Soul, John
+        person(UnlistSplitClean(parts[2L]), c(cleanupLatex(von), cleanupLatex(sub(vonrx, '', parts[1L]))))
+      }
+    }
+  }else if (len.parts == 3L){
+    vonrx <- "^([[:lower:]+[:space:]?]+)[[:space:]]"
+    m <- regexec(vonrx, parts[1L])
+    von <- unlist(regmatches(parts[1L], m))[2]
+    if (is.na(von)){  # e.g. White, Jr., Walter
+      person(UnlistSplitClean(parts[3L]), c(cleanupLatex(parts[1L]), cleanupLatex(parts[2L])))
+    }else{  # e.g. des White, Jr., Walter
+      person(UnlistSplitClean(parts[3L]),
+             c(cleanupLatex(von), cleanupLatex(sub(vonrx, '', parts[1L])), cleanupLatex(parts[2L])))
+    }
+  }else{
+    stop('Invalid name format in bibentry.')
+  }
+}
+
 make.bib.entry <-
 function( x )
 {
@@ -39,10 +159,10 @@ function( x )
     names(y) <- tolower( names(y) )
 
     if( "author" %in% names(y) ){
-        y[["author"]] <- arrange.authors( y[["author"]] )
+        y[["author"]] <- ArrangeAuthors( y[["author"]] )
     }
     if( "editor" %in% names(y) ){
-        y[["editor"]] <- arrange.authors( y[["editor"]] )
+        y[["editor"]] <- ArrangeAuthors( y[["editor"]] )
     }
 
     tryCatch(
