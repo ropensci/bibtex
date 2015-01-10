f414e7598db013f0933a5ade0c9b7aee22f5147d
Romain François <romain@r-enthusiasts.com>
2015-01-10 12:24:26 +0100

    setting up travis

---
+++ inst/clean-bibtex.R
@@ -0,0 +1,14 @@
+lines <- readLines( "bibtex/src/biblex.c" )
+
+# remove declaration of yy_fatal_error
+decl <- grep("static void yy_fatal_error.*;", lines)
+lines[decl] <- paste( "//", lines[decl] )
+
+# first line of definition of yy_fatal_error
+def_begin <- grep("static void yy_fatal_error[^;]*$", lines )
+def_end <- def_begin + grep( "^}", tail(lines, -def_begin) )[1L]
+
+lines[ def_begin:def_end ] <- paste( "// ", lines[def_begin:def_end] )
+
+writeLines( lines, "bibtex/src/biblex.c" )
+
