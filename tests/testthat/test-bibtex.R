66aee4a3165ec8a86a13371f41116336d01a7434
Romain François <romain@r-enthusiasts.com>
2015-01-28 16:11:10 +0100

    case sensitivity

--- tests/testthat/test-bibtex.R
+++ tests/testthat/test-bibtex.R
@@ -6,7 +6,7 @@ test_that("arrange.single.author does not add extra trailing space in given name
 })
 
 test_that("read.bib Ignores entry but does not stop with invalid author/editor (#12)", {
-  f <- file.path(system.file("Bib", "badFormat.bib", package = "bibtex"))
+  f <- file.path(system.file("bib", "badFormat.bib", package = "bibtex"))
   bib <- read.bib(f)
   expect_true(length(bib) == 1L)
 })
