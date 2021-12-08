9aebbc9b9a870361b72cea6de01ec9a075a1ca9f
dieghernan <diego.hernangomezherrero@gmail.com>
2021-12-08 19:36:35 +0100

    Skip on non windows
    Possibly a character problem related with
    https://github.com/ropensci/bibtex/issues/20 and
    https://github.com/ropensci/bibtex/issues/43

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -48,6 +48,8 @@ test_that("Read datasets", {
 test_that("Read graphics", {
   bib <- read.bib(package = "graphics")
 
+  skip_on_os(c("mac", "linux", "solaris"))
+
   expect_snapshot_output(bib)
 
   expect_snapshot_output(toBibtex(bib))
@@ -55,6 +57,8 @@ test_that("Read graphics", {
 
 test_that("Read grDevices", {
   bib <- read.bib(package = "grDevices")
+  
+  skip_on_os(c("mac", "linux", "solaris"))
 
   expect_snapshot_output(bib)
 
