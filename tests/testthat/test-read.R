9dc73c382c8c6886c516fef104f21de1d5e0084f
dieghernan <diego.hernangomezherrero@gmail.com>
2021-12-14 10:22:15 +0000

    Add non standard field names

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -7,3 +7,23 @@ test_that("Read errors 1", {
 test_that("Not find bib on package", {
   expect_error(findBibFile("mockpackage"))
 })
+
+test_that("Test entry with non standard fields", {
+  tmp <- tempfile(fileext = ".bib")
+  entry <- '@Article{newspaper,
+    author = {Van Damme, Jean-Claude},
+    title  = {Article title},
+    date   = {2016-12-21},
+    journal = {Newspaper name},
+    hyphen-field = "This one",
+    under_field = "This one",
+    double:field = "This one",
+  }'
+  writeLines(entry, tmp)
+
+  bib <- read.bib(tmp)
+
+  expect_snapshot_output(bib)
+
+  expect_snapshot_output(toBibtex(bib))
+})
