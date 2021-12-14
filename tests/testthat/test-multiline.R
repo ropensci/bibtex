b2b1417d8f64451c430f6fbd272957c305f7f2bd
dieghernan <diego.hernangomezherrero@gmail.com>
2021-12-14 09:20:44 +0000

    Add test for multiline string

--- tests/testthat/test-multiline.R
+++ tests/testthat/test-multiline.R
@@ -5,3 +5,11 @@ test_that("Multiline from file", {
 
   expect_snapshot_output(toBibtex(out))
 })
+
+test_that("Multiline string", {
+  file <- system.file("bib/multiline_string.bib", package = "bibtex")
+  
+  out <- read.bib(file)
+  
+  expect_snapshot_output(toBibtex(out))
+})
